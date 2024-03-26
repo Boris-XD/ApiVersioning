50bb964ece6728e0736f17094ea23228171fd3da
Boris Sea <seaborisrc@gmail.com>
2024-03-26 10:40:59 -0400

    ApiVersioning - Add options to Swagger Gen

--- Program.cs
+++ Program.cs
@@ -7,7 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "API v1", Version = "v1" });
+    c.SwaggerDoc("v2", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "API v2", Version = "v2" });
+});
 
 var apiVersioningBuilder = builder.Services.AddApiVersioning(o =>
 {
@@ -19,6 +23,7 @@ var apiVersioningBuilder = builder.Services.AddApiVersioning(o =>
         new HeaderApiVersionReader("X-Version"),
         new MediaTypeApiVersionReader("ver"));
 });
+
 
 apiVersioningBuilder.AddApiExplorer(
     options =>
@@ -32,12 +37,15 @@ var app = builder.Build();
 
 
 
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    // app.UseSwagger();
-    // app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+        c.SwaggerEndpoint("/swagger/v2/swagger.json", "API v2");
+    });
 }
 
 app.UseHttpsRedirection();
